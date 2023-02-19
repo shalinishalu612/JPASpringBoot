17afb5e6fb271d0ed5bd0adddc0ba39b603a2034
Shalini <shalini.shalu612@gmail.com>
2023-02-19 23:58:14 +0530

    Using Postman to send req and res

--- src/main/java/com/example/demo/controller/AlienController.java
+++ src/main/java/com/example/demo/controller/AlienController.java
@@ -3,12 +3,16 @@ package com.example.demo.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 import com.example.demo.dao.AlienRepo;
@@ -16,7 +20,7 @@ import com.example.demo.model.Alien;
 
 
 
-@Controller
+@RestController
 
 public class AlienController {
 	
@@ -28,11 +32,12 @@ public class AlienController {
 		return "home.jsp";
 	}
 	
-	@ResponseBody
-	@RequestMapping("/addAlien")
-		public String addAlien(Alien alien) {
+	
+	// @PostMapping(path="/alien",consumes = {"application/json"})
+	@PostMapping("/alien")
+		public Alien addAlien(@RequestBody Alien alien) {
 			repo.save(alien);
-			return "Succesfully Added";
+			return alien;
 		}
 	
 	// @RequestMapping("/getAlien")
@@ -46,15 +51,29 @@ public class AlienController {
 
 
 	
-	@RequestMapping("/aliens")
-	@ResponseBody
+	@GetMapping("/aliens")
+	
 	public List<Alien> getAllAliens(){
-		return (List<Alien>) repo.findAll();
+		return repo.findAll();
 	}
 
-	@RequestMapping("/alien/{aid}")
-	@ResponseBody
+	@GetMapping(path="/alien/{aid}")
+	
 	public Optional<Alien> getAlien(@PathVariable("aid") int aid){
 		return repo.findById(aid);
 	}
+
+	@DeleteMapping("/alien/{aid}")
+	public String deleteAlien(@PathVariable int aid){
+		Alien a = repo.getOne(aid);
+		repo.delete(a);
+		return "deleted";
+	}
+
+	@PutMapping(path = "/alien",consumes = {"application/json"})
+	public Alien SaveOrUpdateAlien(@RequestBody Alien alien){
+		repo.save(alien);
+		return alien;
+	}
+
 }
--- src/main/java/com/example/demo/model/Alien.java
+++ src/main/java/com/example/demo/model/Alien.java
@@ -9,36 +9,37 @@ public class Alien {
 	private int aid;
 	private String aname;
 	private String tech;
-
-	
-	public void setAid(int aid) {
-		this.aid = aid;
-	}
-	
-	public void setAname(String aname) {
-		this.aname = aname;
-	}
-
-	public void setAtech(String tech) {
-		this.tech = tech;
-	}
-
 	public int getAid() {
 		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
 	}
 	public String getAname() {
 		return aname;
 	}
-	public String getAtech() {
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	public String getTech() {
 		return tech;
 	}
-	
-	
-	
+	public void setTech(String tech) {
+		this.tech = tech;
+	}
 	@Override
 	public String toString() {
-		return "Alien [aid=" + aid + ", aname=" + aname + "]";
+		return "Alien [aid=" + aid + ", aname=" + aname + ", tech=" + tech + "]";
 	}
+
 	
-	
+
 }
+
+	
+
+	
+	
+	
+	
+	
